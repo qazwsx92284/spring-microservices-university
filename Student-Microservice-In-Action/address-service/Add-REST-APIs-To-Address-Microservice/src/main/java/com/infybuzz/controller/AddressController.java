24510d3673666f921a7fe68d5e788d9fb0442650
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-05-21 21:33:51 -0700

    student ms- update address - in progress

---
+++ Student-Microservice-In-Action/address-service/Add-REST-APIs-To-Address-Microservice/src/main/java/com/infybuzz/controller/AddressController.java
@@ -0,0 +1,34 @@
+package com.infybuzz.controller;
+
+import com.infybuzz.request.CreateAddressRequest;
+import com.infybuzz.response.AddressResponse;
+import com.infybuzz.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/address")
+public class AddressController {
+
+	@Autowired
+	AddressService addressService;
+
+	@PostMapping("/create")
+	public AddressResponse createAddress (@RequestBody CreateAddressRequest createAddressRequest) {
+		return addressService.createAddress(createAddressRequest);
+	}
+	
+	@GetMapping("/getById/{id}")
+	public AddressResponse getById(@PathVariable long id) {
+		return addressService.getById(id);
+	}
+
+	@GetMapping("/addresses")
+	public ResponseEntity<Flux<AddressResponse>> getAddresses() {
+		return new ResponseEntity(addressService.getAllAddresses(), HttpStatus.OK);
+	}
+	
+}
